f6cab9bc2ead48055d72d08319e9912415fc0f64
Lev <Konovalovlev7777@gmail.com>
2025-01-27 15:02:50 +0300

    Initial commit

---
+++ zadanie 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Screen screen = new Screen();
+            screen.OnSelect();
+            screen.Refresh();
+        }
+    }
+    class Screen : ISelectable , IUpdatable 
+    {
+        public Screen() { }
+
+        public void OnSelect()
+        {
+            Console.WriteLine("Выводит экран");
+        }
+
+        public void Refresh()
+        {
+            Console.WriteLine("Экран обновился");
+        }
+    }
+    interface ISelectable 
+    {
+        void OnSelect();
+    }
+    interface IUpdatable
+    {
+        void Refresh();
+    }
+}
